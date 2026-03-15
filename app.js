69044c2f1b61efab8a4762d7df19a80591e73f70
Sara Zapata <sara.zapata01@gmail.com>
2026-03-14 20:10:09 -0500

    semaneja el tema de cors para vercel

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const swaggerSpec = require('./swagger');
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://frontend-proyecto-bit.vercel.app/', // o '*' temporal para pruebas
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
